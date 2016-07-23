558285334df1bc7c6a4ad9ba1121591481dc3be2
Berkeley Martinez <Berkeley@robotie.com>
2016-07-23 12:17:40 -0700

    feature: add gh-pages deploy

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -11,7 +11,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, "dist"),
     filename: "bundle.js",
-    publicPath: "/dist/"
+    publicPath: "/nodeschoolsf-july/"
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
